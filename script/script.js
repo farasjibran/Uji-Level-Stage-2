0ad5d3006821bc7de31e393c3a28642a93e7213a
Muhammad Farras Jibran <farasjibran@gmail.com>
2019-12-04 08:42:42 +0700

    Update 1.2

--- script/script.js
+++ script/script.js
@@ -1,6 +1,6 @@
 // Jquery document ready
 $(document).ready(function () {
-    loopMenu()
+    LoopMenu()
 });
 
 
@@ -10,8 +10,14 @@ $('.account').text(name)
 
 // Holder
 const menuHolder = document.getElementById("menuHolder")
-const orderHolder = document.getElementById("orderHolder")
+const orderanHolder = document.getElementById("orderanHolder")
 
+// let untuk menghitung hasil
+let subTotal = 0
+let totalHasil = 0
+let tax = 0
+
+// array untuk menyimpan data yang sudah diclick
 let orderItem = []
 
 // Item menu di dalam array
@@ -48,7 +54,6 @@ let menuItem = [
         "gambar_item": "https://cdn-brilio-net.akamaized.net/news/2019/10/07/171868/1107923-10-resep-ayam-geprek-crispy-enak.jpg"
     },
 
-
     // List makanan ringan
     {
         "Kategori": "MakananRingan",
@@ -74,12 +79,6 @@ let menuItem = [
         "harga": 12000,
         "gambar_item": "https://media.travelingyuk.com/wp-content/uploads/2018/07/Kue-Pancong-Rawa-Belong.jpg"
     },
-    {
-        "Kategori": "MakananRingan",
-        "nama_item": "Waffle + Ice cream",
-        "harga": 19000,
-        "gambar_item": "https://food.fnr.sndimg.com/content/dam/images/food/fullset/2018/11/27/DV2907_BBB%20Bacon-in-the-Batter-Buttermilk-Waffle-Bourbon-Ice-Cream-Bacon-Toffee_s4x3.jpg.rend.hgtvcom.826.620.suffix/1543329865056.jpeg"
-    },
 
     // List minuman
     {
@@ -94,6 +93,12 @@ let menuItem = [
         "harga": 7000,
         "gambar_item": "https://www.errenskitchen.com/wp-content/uploads/2014/08/lemon-Iced-Tea.jpg"
     },
+    {
+        "Kategori": "Minuman",
+        "nama_item": "Coffe latte",
+        "harga": 12000,
+        "gambar_item": "https://majalah.ottencoffee.co.id/wp-content/uploads/2015/07/shutterstock_223511062.jpg"
+    },
 
     // List Dessert
     {
@@ -102,10 +107,22 @@ let menuItem = [
         "harga": 9000,
         "gambar_item": "https://food.fnr.sndimg.com/content/dam/images/food/fullset/2019/5/17/0/WU2207_Mermaid-Ice-Cream_s4x3.jpg.rend.hgtvcom.826.620.suffix/1558112854371.jpeg"
     },
+    {
+        "Kategori": "Dessert",
+        "nama_item": "Waffle + Ice cream",
+        "harga": 19000,
+        "gambar_item": "https://food.fnr.sndimg.com/content/dam/images/food/fullset/2018/11/27/DV2907_BBB%20Bacon-in-the-Batter-Buttermilk-Waffle-Bourbon-Ice-Cream-Bacon-Toffee_s4x3.jpg.rend.hgtvcom.826.620.suffix/1543329865056.jpeg"
+    },
+    {
+        "Kategori": "Dessert",
+        "nama_item": "Pancake",
+        "harga": 14000,
+        "gambar_item": "https://www.handletheheat.com/wp-content/uploads/2019/02/The-Best-Pancake-Recipe-SQUARE.jpg"
+    },
 ]
 
 // Loop Kategori
-function loopMenu(Kategori) {
+function LoopMenu(Kategori) {
     $('#mknberatHolder').html("")
     $('#mknringanHolder').html("")
     $('#minumanHolder').html("")
@@ -150,37 +167,50 @@ function loopMenu(Kategori) {
 
 // Fuction Holder
 function ItemHolder(data, nama_item, harga, gambar_item) {
-    const ItemHolder = `<div onclick="MenuClicked(${data})" class="card-group sorot col-md-3 p-3"> <div class="card text-center" style="border: none"> <div class="text-center"> <img style="width: 130px; height:130px; border-radius: 100%" src="${gambar_item}" class="card-img-top" alt=""> </div> <div class="card-body p-0 pt-2"> <h4 class="mb-1 card-title">${nama_item}</h4> </div> <div class="card-body text-secondary p-0"> <p class=" card-title">Rp <span>${harga}</span></p> </div> </div> </div>`
-    return ItemHolder;
+    const itemHolder = `<div onclick="MenuClicked(${data})" class="card-group efek col-md-3 p-3"> <div class="card text-center" style="border: none"> <div class="text-center"> <img style="width: 130px; height:130px; border-radius: 100%" src="${gambar_item}" class="card-img-top" alt=""> </div> <div class="card-body p-0 pt-2"> <h4 class="mb-1 card-title">${nama_item}</h4> </div> <div class="card-body text-secondary p-0"> <p class=" card-title">Rp <span>${harga}</span></p> </div> </div> </div>`
+    return itemHolder;
 }
 
+// Fuction apabila menu diclick
 function MenuClicked(nama_item, harga, gambar_item) {
     console.log("menu start")
     for (let i = 0; i < orderItem.length; i++) {
-        if (orderItem[i]["nama_item"] == nama) {
-            orderItem[i]["qty"] += 1
+        if (orderItem[i]["nama_item"] == nama_item) {
+            orderItem[i]["kelipatan"] += 1
             LoopOrder()
             return
         }
     }
-
     let order = {
         "nama_item": nama_item,
         "harga": harga,
         "gambar_item": gambar_item,
-        "qty": 1,
+        "kelipatan": 1,
     }
     orderItem.push(order)
     LoopOrder()
 }
 
+// Function untuk menampilkan menu yang sudah diclick
 function LoopOrder() {
-    orderHolder.innerHTML = ""
-
+    $('#orderanHolder').html("")
     for (let i = 0; i < orderItem.length; i++) {
-        $("#orderHolder").append(' <div class="media p-2 mb-1"> <img src="' + orderItem[i].getgambar + '" class="gambar-item mr-3"alt="..."><div class="media-body"><h5 class="mt-0 mb-0">' + orderItem[i].getnama + '</h5><p style="margin: 0">Rp <span id="harga">' + orderItem[i].getharga + '</span></p> <span class="badge badge-success">' + orderItem[i].getkelipatan + '</span></div><div> <br><h5 class="mb-0 mr-1">Rp <span id="totalQty">' + (orderItem[i].getharga * orderItem[i].getkelipatan) + '</span></h5></div><div class="destroy"><img src="img/Icon ionic-ios-close-circle.png" alt="" class="closedimg"></div></div>');
+        const itemHolder = `
+       <div class="media p-2 mb-1"> 
+        <img src="${orderItem[i]["gambar_item"]}" class="mr-2" alt="..." style="width: 100px; border-radius: 100%; height: 100px"> 
+        <div class="media-body"> 
+        <h5 class="mt-0 mb-0">${orderItem[i]["nama_item"]}</h5> 
+        <p style="margin: 0">Rp <span>${orderItem[i]["harga"]}</span></p> <span class="badge badge-success">x${orderItem[i]["kelipatan"]}</span> 
+        </div> 
+        <div> <br> 
+        <h5 class="mb-0 mr-1">Rp <span id="totalQty">${(orderItem[i]["harga"] * orderItem[i]["kelipatan"])}</span></h5> 
+        </div> 
+        <div class="destroy"> 
+            <img src="img/Icon ionic-ios-close-circle.png" class="mr-3" alt="..." style="width: 20px"> 
+        </div> </div>`
+        $("#orderanHolder").append(itemHolder);
     }
-    Count()
+    Counting()
     OnDelete();
 }
 
@@ -193,20 +223,19 @@ function OnDelete() {
     }
 }
 
-function Count() {
+function Counting() {
     if (orderItem.length >= 1) {
-        let itungCount = 0
+        subTotal = 0
         for (let i = 0; i < orderItem.length; i++) {
-            let harga = Number(orderItem[i].getharga)
-            let kelipatan = Number(orderItem[i].getkelipatan)
-            itungCount += harga * kelipatan
+            let menghitung = (orderItem[i]["harga"] * orderItem[i]["kelipatan"])
+            subTotal += menghitung
         }
-        let pajak = itungCount * 10 / 100
-        let totalCount = itungCount + pajak
+        tax = subTotal * 10 / 100
+        totalHasil = subTotal + tax
 
-        $("#subTotal").text(itungCount)
-        $("#tax").text(pajak)
-        $("#total").text(totalCount)
+        $("#subTotal").text(subTotal)
+        $("#tax").text(tax)
+        $("#total").text(totalHasil)
     } else if (orderItem.length == 0) {
         $("#subTotal").text(0)
         $("#tax").text(0)
@@ -214,6 +243,26 @@ function Count() {
     }
 };
 
-function Payment() {
-    $('#payment')
-}
+$("#btnpayment").click(function () {
+    console.log("payment : Clicked")
+    $("#paymentHolder").html("")
+    for (let i = 0; i < orderItem.length; i++) {
+        const itemHolder = `
+        <div class="media p-2 mb-1">
+        <div class="media-body d-flex">
+            <h5 class="col-8 align-baseline mt-0 mb-0">${orderItem[i]["nama_item"]}</h5>
+            <h6 class=""><span class="badge align-baseline badge-success">x${orderItem[i]["kelipatan"]}</span></h6>
+            <h6 class="col-sm align-baseline mb-0 ml-2">Rp ${(orderItem[i]["harga"] * orderItem[i]["kelipatan"])}</}</h6>
+        </div>
+        </div>`
+        $("#paymentHolder").append(itemHolder)
+    }
+    $("#TotalSub").text(subTotal)
+    $("#Denda").text(tax)
+    $("#Total").text(totalHasil)
+})
+
+$("#btnBayar").on('click', function () {
+    alert("Terimakasih telah membeli")
+    location.reload()
+})
